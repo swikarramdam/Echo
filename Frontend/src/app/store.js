24e6e3b06b3359fa085e3fb10d2c327d32d2d350
swikarramdam <swikarramdam@gmail.com>
2025-09-24 10:35:12 +0545

    contains backend only

--- Frontend/src/app/store.js
+++
@@ -1,9 +0,0 @@
-//app/store.js
-import { configureStore } from "@reduxjs/toolkit";
-import roomsReducer from "../features/rooms/roomsSlice";
-export const store = configureStore({
-  reducer: {
-    rooms: roomsReducer,
-  },
-});
-export default store;
